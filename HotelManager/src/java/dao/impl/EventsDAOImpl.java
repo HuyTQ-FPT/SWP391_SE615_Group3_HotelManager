50fd1457d148dea3a591ac1856588964059ec812
HieuZozo <89286810+HiEu061201@users.noreply.github.com>
2022-07-11 22:01:57 +0700

    fix

--- HotelManager/src/java/dao/impl/EventsDAOImpl.java
+++ HotelManager/src/java/dao/impl/EventsDAOImpl.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao.impl;
 
 import context.DBContext;
 import dao.EventsDAO;
 import entity.Events;
+import entity.User;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
