438bfa42b0123b25972cb3e152b2673d9e7c606e
NTDno1 <90979335+NTDno1@users.noreply.github.com>
2022-07-19 00:04:20 +0700

    update

--- HotelManager/src/java/dao/impl/RoomCategoryDAOImpl.java
+++ HotelManager/src/java/dao/impl/RoomCategoryDAOImpl.java
@@ -166,7 +166,7 @@ public class RoomCategoryDAOImpl extends DBContext implements RoomCategoryDAO {
      */
     @Override
     public void deleteRoomCategory(String roomcate) throws Exception {
-        String query = "update Room set RoomcateID = replace(RoomcateID,?,6); delete from CateRoom where RoomcateID = ?";
+        String query = "update Room set RoomcateID = replace(RoomcateID,?,6);update Room set Status = replace(Status,0,1) ; delete from CateRoom where RoomcateID = ?";
         PreparedStatement pre = null;
         try {
             pre = conn.prepareStatement(query);
