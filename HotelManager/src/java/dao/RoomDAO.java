ae8fd99e93d4fee108d68b14c988d0f5ba8c84fb
Hoang Trong Hieu <hieu0904652125@gmail.com>
2022-07-19 05:20:39 +0700

    comment

--- HotelManager/src/java/dao/RoomDAO.java
+++ HotelManager/src/java/dao/RoomDAO.java
@@ -7,6 +7,7 @@
  * DATE          Version    Author           DESCRIPTION
  *               1.0                         First Deploy
  * 13/07/2022    1.0        HieuLBM          Comment
+ * 18/07/2022    1.0        HieuHT           Comment
  */
 package dao;
 
@@ -88,34 +89,111 @@ public interface RoomDAO {
 
     public Vector<Room> getRoomList(String sql) throws Exception;
 
+    /**
+     * count Rooms from Room table
+     *
+     * @return int
+     * @throws java.lang.Exception
+     */
     public int getPage() throws Exception;
- 
+
+    /**
+     * Show Room by page from Room table
+     *
+     * @param n
+     * @return <code>Room</code> object.
+     * @throws java.lang.Exception
+     */
     public Vector<Room> getRoomByPage(int n) throws Exception;
 
+    /**
+     * Show Rooms by page and status from Room table
+     *
+     * @param n
+     * @return <code>Room</code> object.
+     * @throws java.lang.Exception
+     */
     public Vector<Room> getRoomByPageStatus(int n) throws Exception;
 
+    /**
+     * count Rooms by status from Room table
+     *
+     * @return int
+     * @throws java.lang.Exception
+     */
     public int getPageByPageStatus() throws Exception;
 
     public Room getRoom(String roomid) throws Exception;
- 
+
     public void crudRoom(String sql) throws Exception;
 
     public void updateRoom(Room Room) throws Exception;
 
     public void deleteRoom(int roomid) throws Exception;
 
+    /**
+     * count Rooms by Price from Room table
+     *
+     * @param p1
+     * @param p2
+     * @return int
+     * @throws java.lang.Exception
+     */
     public int getPageByPrice(int p1, int p2) throws Exception;
 
+    /**
+     * Show Rooms by price between from Room table
+     *
+     * @param n
+     * @param from
+     * @param to
+     * @return <code>Room</code> object.
+     * @throws java.lang.Exception
+     */
     public Vector<Room> getRoomListbyPrice(int n, int from, int to) throws Exception;
 
+    /**
+     * Show Rooms by price max from Room table
+     *
+     * @param n
+     * @return <code>Room</code> object.
+     * @throws java.lang.Exception
+     */
     public Vector<Room> getRoomByPriceMax(int n) throws Exception;
+
+    /**
+     * Show Rooms by price min from Room table
+     *
+     * @param n
+     * @return <code>Room</code> object.
+     * @throws java.lang.Exception
+     */
 
     public Vector<Room> getRoomByPriceMin(int n) throws Exception;
 
+    /**
+     * Show Rooms by Rate from Room table
+     *
+     * @param n
+     * @return <code>Room</code> object.
+     * @throws java.lang.Exception
+     */
+
     public Vector<Room> getRoomByRate(int n) throws Exception;
+
+    /**
+     * search Rooms by date check in check out from Room table
+     *
+     * @param a
+     * @param datein
+     * @param dateout
+     * @return <code>Room</code> object.
+     * @throws java.lang.Exception
+     */
 
     public Vector<RoomByDate> seachRoom(String a, String datein, String dateout) throws Exception;
 
     public int getPage(String sql) throws Exception;
+
     public Vector<Room> getRoomList1(String sql) throws Exception;
 }
