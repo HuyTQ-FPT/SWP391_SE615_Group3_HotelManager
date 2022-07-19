05a0c4ff2cd258c2bd5a274e1fe79b6904b331fb
NTDno1 <90979335+NTDno1@users.noreply.github.com>
2022-07-19 13:09:25 +0700

    Update

--- HotelManager/src/java/dao/ServiceDAO.java
+++ HotelManager/src/java/dao/ServiceDAO.java
@@ -15,23 +15,69 @@ import entity.Service;
 import java.util.Vector;
 
 public interface ServiceDAO {
-
+    /**
+     * Get all Service from database
+     *
+     * @return list service <code>Service</code> objects
+     * <code>java.util.Vector</code> object
+     * @throws java.lang.Exception
+     */
     public Vector<Service> getServiceList();
-
+    /**
+     * Get Service from database
+     *
+     * @return a list of <code>Service</code> objects
+     * <code>java.util.Vector</code> object
+     * @throws java.lang.Exception
+     */
     public Vector<Service> getServiceListbyran();
-
+    /**
+     * Get Service from database
+     * @throws java.lang.Exception
+     */
     public Service getServicedetail(String sid) throws Exception;
-
+    /**
+     * Get Service detail from database
+     * @throws java.lang.Exception
+     */
     public void insertService(String ServiceName, String ServiceImage, String ServiceDes, String ServicePrice) throws Exception;
-
+    /**
+     * Insert Service from database
+     * @throws java.lang.Exception
+     */
     public void updateService(String ServiceName, String ServiceImage, String ServiceDes, String ServicePrice, String ServiceID) throws  Exception;
-
+    /**
+     * Update Service from database
+     * @throws java.lang.Exception
+     */
     public void deleteService(String sql) throws Exception;
-
+    /**
+     * Delete Service from database
+     * @throws java.lang.Exception
+     */
     public Vector<FeedBackService> getFeedBackBySeviceID(String Sql) throws Exception;
-
+    /**
+     * Get all Blog from database
+     * @return a list of <code>FeedBackService</code> objects
+     * <code>java.util.FeedBackService</code> object
+     * @throws java.lang.Exception
+     */
     public void insertCommentService(String ServiceID, String AccountID, String Comment) throws Exception;
+        /**
+     * InsertCommnet from database
+     * @throws java.lang.Exception
+     */
     public void BlockComnent(String CommentID) throws Exception;
+        /**
+     * block comment from database
+     * @throws java.lang.Exception
+     */
     public void UnblockComment(String CommentID) throws Exception;
+        /**
+     * Unblock comment from database
+     *
+     * @return a list of <code>Service</code> objects
+     * @throws java.lang.Exception
+     */
     public Service getLastService()  throws Exception;
 }
--- HotelManager/src/java/filter/AdminFilter.java
+++ HotelManager/src/java/filter/AdminFilter.java
@@ -30,7 +30,7 @@ import javax.servlet.http.HttpSession;
  *
  * @author Minh Hiếu
  */
-@WebFilter(filterName = "AdminFilter", urlPatterns = {"/AdminController","/RequestController","/ManagerAccount"})
+@WebFilter(filterName = "AdminFilter", urlPatterns = {"/AdminController","/RequestController","/ManagerAccount","/RoomsController","/ImageController","/DeviceController"})
 public class AdminFilter implements Filter {
 
     private static final boolean debug = true;
